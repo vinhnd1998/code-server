3800d91e4f2512064328f92c907e104e00c11fea
ndvinh98 <kaiznguyen98@gmail.com>
2023-03-27 02:12:14 +0000

    chore: update build

--- ci/build/build-vscode.sh
+++ ci/build/build-vscode.sh
@@ -95,7 +95,7 @@ main() {
         "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
         "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
         "itemUrl": "https://marketplace.visualstudio.com/items",
-        "controlUrl:"",
+        "controlUrl":"",
         "recommendationsUrl": ""
     },
     "aiConfig": {
